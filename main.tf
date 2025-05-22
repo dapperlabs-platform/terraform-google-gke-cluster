5409627ff6e6fd00d789dcf49d214225330dc3f3
Alex Grieco <awgrieco@gmail.com>
2025-05-22 14:56:50 -0700

    Adding GSM secrets for cluster details

--- main.tf
+++ main.tf
@@ -346,3 +346,32 @@ resource "kubernetes_storage_class" "example" {
     network = "gke-application-cluster-vpc"
   }
 }
+
+# Create GSM secret with Cluster details -> used to register cluster in ArgoCD more easily
+# The cluster ca certificate
+resource "google_secret_manager_secret" "gke_cluster_ca" {
+  secret_id = "gke-cluster-${var.location}-ca-b64" # The name of your secret
+  project   = var.project_id
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "gke_cluster_ca" {
+  secret      = google_secret_manager_secret.gke_cluster_ca.id
+  secret_data = google_container_cluster.cluster.master_auth[0].cluster_ca_certificate
+}
+
+# The clusters public endpoint
+resource "google_secret_manager_secret" "gke_cluster_endpoint" {
+  secret_id = "gke-cluster-${var.location}-endpoint" # The name of your secret
+  project   = var.project_id
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "gke_cluster_endpoint" {
+  secret      = google_secret_manager_secret.gke_cluster_endpoint.id
+  secret_data = google_container_cluster.cluster.endpoint
+}
